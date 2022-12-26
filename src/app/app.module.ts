dd239f9ce2c8768326eb7a945c1a082c174769d7
leslie <sarybell28@gmail.com>
2022-12-26 17:15:54 -0500

    precios

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,16 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { TitleComponent } from './title/title.component';
+import { CardsComponent } from './cards/cards.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    TitleComponent,
+    CardsComponent
   ],
   imports: [
     BrowserModule
